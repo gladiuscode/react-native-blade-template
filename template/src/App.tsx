66ab3de1265220d413d4240e1fb9dfc755b138da
gladius <gladiusservices@protonmail.com>
2022-04-01 19:53:23 +0200

    Initialize react native project

---
+++ template/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text style={{textAlign: 'center'}}>BLADE TEMPLATE</Text>
+    </View>
+  );
+};
+
+export default App;
