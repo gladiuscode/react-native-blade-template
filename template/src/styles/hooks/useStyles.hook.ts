15a5486cbb84cce34f582672c0eb738a388071dc
gladius <gladiusservices@protonmail.com>
2022-04-01 22:05:33 +0200

    Enhance useStyles hook readability and code quality

--- template/src/styles/hooks/useStyles.hook.ts
+++ template/src/styles/hooks/useStyles.hook.ts
@@ -3,15 +3,15 @@ import {StyleSheet} from 'react-native';
 import {StylesProviderValue} from '../provider/styles.types';
 import {StylesContext} from '../provider/styles.data';
 
-type Builder = (
+export type StylesBuilder = (
   defaultStyles: StylesProviderValue,
 ) => ReturnType<typeof StyleSheet.create>;
 
-export const useStyles = (builder: Builder) => {
+export const useStyles = (builder: StylesBuilder) => {
   const defaultStyles = useContext<StylesProviderValue>(StylesContext);
   const styles = builder(defaultStyles);
   return useMemo(
-    () => ({styles, onToggleTheme: defaultStyles.onThemeChange}),
+    () => ({styles, onToggleChange: defaultStyles.onThemeChange}),
     [defaultStyles.onThemeChange, styles],
   );
 };
