db8404ad0c8a962bb1fd7824335f3047070c598f
gladius <gladiusservices@protonmail.com>
2022-04-01 22:06:09 +0200

    Wrap App with StylesProvider

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './navigation/stacks/main.stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import StylesProvider from './styles/provider/styles.provider';
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <MainStack />
-      </NavigationContainer>
+      <StylesProvider>
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
+      </StylesProvider>
     </SafeAreaProvider>
   );
 };
