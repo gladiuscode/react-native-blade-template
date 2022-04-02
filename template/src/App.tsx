402e992928cde243d8e0abbc1e843e9bc4008cbc
gladius <gladiusservices@protonmail.com>
2022-04-02 10:59:16 +0200

    Wrap App with LocalizationProvider

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -3,14 +3,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './navigation/stacks/main.stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import StylesProvider from './styles/provider/styles.provider';
+import LocalizationProvider from './localization/provider/localization.provider';
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
       <StylesProvider>
-        <NavigationContainer>
-          <MainStack />
-        </NavigationContainer>
+        <LocalizationProvider>
+          <NavigationContainer>
+            <MainStack />
+          </NavigationContainer>
+        </LocalizationProvider>
       </StylesProvider>
     </SafeAreaProvider>
   );
