7b6470f2a787b9c8b993b38866e7c1e5e6e11540
gladius <gladiusservices@protonmail.com>
2022-04-03 13:10:34 +0200

    Create ApiLayer class with basic AxiosRequestConfig setters

--- template/src/api/apiLayer.api.ts
+++ template/src/api/apiLayer.api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, {AxiosRequestConfig} from 'axios';
 
 const instance = axios.create();
 
@@ -15,3 +15,46 @@ const initInstanceDefaults = () => {
 };
 
 initInstanceDefaults();
+
+class ApiLayer<RequestData> {
+  private readonly url?: AxiosRequestConfig<RequestData>['url'];
+  private readonly baseURL?: AxiosRequestConfig<RequestData>['baseURL'];
+  private method: AxiosRequestConfig<RequestData>['method'];
+  private data: AxiosRequestConfig<RequestData>['data'];
+  private headers: AxiosRequestConfig<RequestData>['headers'];
+
+  constructor({
+    url,
+    baseURL,
+    method,
+    data,
+    headers,
+  }: AxiosRequestConfig<RequestData>) {
+    this.url = url;
+    this.baseURL = baseURL;
+    this.method = method;
+    this.data = data;
+    this.headers = headers;
+  }
+
+  setMethod(method: AxiosRequestConfig<RequestData>['method']) {
+    this.method = method ?? this.method;
+    return this;
+  }
+
+  setData(data: AxiosRequestConfig<RequestData>['data']) {
+    this.data = data ?? this.data;
+    return this;
+  }
+
+  setHeaders(headers: AxiosRequestConfig<RequestData>['headers']) {
+    const baseHeaders = this.headers ?? {};
+    this.headers = {
+      ...baseHeaders,
+      ...headers,
+    };
+    return this;
+  }
+}
+
+export default ApiLayer;
